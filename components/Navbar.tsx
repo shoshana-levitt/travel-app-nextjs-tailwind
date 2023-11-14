701e23cae5e074f96e3588a8400690cb0a032301
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-14 17:26:25 -0500

    add constants folder, add ul in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,6 +8,13 @@ const Navbar = () => {
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key}>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
   );
 };
