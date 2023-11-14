14e4fc259e22a1413c81d94fc48f9c7f55d01cc3
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-14 17:17:13 -0500

    add new favicon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
